e96abf0c90ca81894a87935c95fa5375c0b10ed3
Altamiro Figueiredo <altamironeto03@hotmail.com>
2022-06-14 19:04:05 -0300

    Entities

---
+++ src/main/java/com/gvendas/gestaovendas/entities/Produto.java
@@ -0,0 +1,92 @@
+package com.gvendas.gestaovendas.entities;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.Objects;
+
+@Entity
+@Table(name = "produto")
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "descricao")
+    private String descricao;
+
+    @Column(name = "quantidade")
+    private Integer quantidade;
+
+    @Column(name = "preco_custo")
+    private BigDecimal precoCusto;
+
+    @Column(name = "preco_venda")
+    private BigDecimal precoVenda;
+
+    @Column(name = "observacao")
+    private String observacao;
+
+    @ManyToOne
+    @JoinColumn(name = "codigo_categoria", referencedColumnName = "codigo")
+    private Categoria categoria;
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public BigDecimal getPrecoCusto() {
+        return precoCusto;
+    }
+
+    public void setPrecoCusto(BigDecimal precoCusto) {
+        this.precoCusto = precoCusto;
+    }
+
+    public BigDecimal getPrecoVenda() {
+        return precoVenda;
+    }
+
+    public void setPrecoVenda(BigDecimal precoVenda) {
+        this.precoVenda = precoVenda;
+    }
+
+    public String getObservacao() {
+        return observacao;
+    }
+
+    public void setObservacao(String observacao) {
+        this.observacao = observacao;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Produto)) return false;
+        Produto produto = (Produto) o;
+        return Objects.equals(getDescricao(), produto.getDescricao()) && Objects.equals(getQuantidade(), produto.getQuantidade()) && Objects.equals(getPrecoCusto(), produto.getPrecoCusto()) && Objects.equals(getPrecoVenda(), produto.getPrecoVenda()) && Objects.equals(getObservacao(), produto.getObservacao()) && Objects.equals(getCategoria(), produto.getCategoria());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getDescricao(), getQuantidade(), getPrecoCusto(), getPrecoVenda(), getObservacao(), getCategoria());
+    }
+}
